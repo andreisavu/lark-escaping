e139b28b8e5fd8f749d9c4dd3c96f142c6fa580a
Andrei Savu <savu.andrei@gmail.com>
2024-06-15 11:56:16 -0700

    Add basic Lark grammar and tests for string literals

---
+++ tests/test_string_literals.py
@@ -0,0 +1,12 @@
+import pytest
+from lark import Lark
+import os
+
+def test_string_literal_parsing():
+    grammar_path = os.path.join(os.path.dirname(__file__), '..', 'grammar.lark')
+    with open(grammar_path) as grammar_file:
+        grammar = grammar_file.read()
+    parser = Lark(grammar, start='string')
+    test_string = '"hello"'
+    parse_tree = parser.parse(test_string)
+    assert str(parse_tree) == "Tree(string, [Token(STRING, '\"hello\"')])"
