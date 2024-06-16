af08656bb03c0524ad66f23074f42fb2a6bbec54
Andrei Savu <savu.andrei@gmail.com>
2024-06-15 19:13:02 -0700

    Define current folder as mydsl library

---
+++ mydsl/__init__.py
@@ -0,0 +1,1 @@
+# This __init__.py file makes mydsl a Python package
---
+++ mydsl/example.py
@@ -0,0 +1,6 @@
+# Example usage of the mydsl library
+
+def greet(name):
+    """Function to greet a person by name."""
+    return f"Hello, {name}!"
+
