374d49844a9c30f5d560273ea1643893f63269d3
Andrei Savu <savu.andrei@gmail.com>
2024-06-15 19:07:03 -0700

    Extract MyEvaluator into mydsl library

---
+++ mydsl/my_evaluator.py
@@ -0,0 +1,14 @@
+from lark import Transformer
+
+class MyEvaluator(Transformer):
+    def string(self, s):
+        # Remove the surrounding quotes and unescape
+        return s[0][1:-1].encode().decode('unicode_escape')
+    
+    def collection_name(self, s):
+        return s[0]
+
+    def get_expression(self, items):
+        # Extract the collection name and the key from the get_expression
+        collection_name, key = items
+        return {'collection_name': collection_name, 'key': key}
